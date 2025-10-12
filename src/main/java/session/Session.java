4db51d7a1b793ecb069036516ffcd77c3f3d28cd
Jovana Jankovic <jankovicj297@gmail.com>
2025-10-13 00:16:55 +0200

    Klasa Session za komunikaciju sa serverom

--- src/main/java/session/Session.java
+++ src/main/java/session/Session.java
@@ -1,21 +1,31 @@
 package session;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 import rs.ac.bg.fon.poslasticarnica.Administrator;
 
 public class Session {
-	
+
 	private static Session instance;
 	private Socket socket;
 	private Administrator ulogovani;
+	private PrintWriter out; // slanje json zahteva serveru
+	private BufferedReader in; // primanje json odgovora sa servera
 
 	private Session() {
 		try {
-			socket = new Socket("localhost", 10000);
+			socket = new Socket("localhost", 10000); // za komunikaciju sa serverom
+
+			this.out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+			this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
 		} catch (IOException ex) {
-			ex.printStackTrace();
+			System.err.println("Greska pri uspostavljanju konekcije ili kreiranju streamova: " + ex.getMessage());
 		}
 	}
 
@@ -24,6 +34,14 @@ public class Session {
 			instance = new Session();
 		}
 		return instance;
+	}
+
+	public PrintWriter getWriter() {
+		return out;
+	}
+
+	public BufferedReader getReader() {
+		return in;
 	}
 
 	public Socket getSocket() {
